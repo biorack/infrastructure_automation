fac9517802d6183b11d89798d9e3bdcbfb126dfb
tharwood3 <tharwood@lbl.gov>
2023-11-03 13:24:58 -0700

    chore: formatting

--- instrument_storage_sync/auto_git_pull.ps1
+++ instrument_storage_sync/auto_git_pull.ps1
@@ -1,22 +1,24 @@
 # Define the GitHub repository URL
 $repositoryUrl = "https://github.com/biorack/infrastructure_automation"
 
-# Define the local directory where you want to clone or update the repository
+# Define the local directory where you want to clone the repository
 $localDirectory = "C:\Scripts\"
 
-# Location of repository if installed
+# Location of local repository
 $localRepository = Join-Path -Path $LocalDirectory -ChildPath "infrastructure_automation"
 
-# Check if the local directory exists; if not, clone the repository
+# Check if the local repository exists; if not, clone the repository into directory
 if (-not (Test-Path -Path $localRepository -PathType Container)) {
 
-	# Change to the local directory
+    # Change to the local directory
     Set-Location -Path $localDirectory
 	
     # Clone the repository
     git clone $repositoryUrl
+
 } else {
-    # Change to the local directory
+
+    # Change to the local repository
     Set-Location -Path $localRepository
 
     # Pull updates from the main branch
