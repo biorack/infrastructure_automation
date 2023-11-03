9c294433c10dfdee8f00bd37d5a05581fe82b800
tharwood3 <tharwood@lbl.gov>
2023-11-03 13:21:06 -0700

    fix: auto git pull

--- instrument_storage_sync/auto_git_pull.ps1
+++ instrument_storage_sync/auto_git_pull.ps1
@@ -2,15 +2,22 @@
 $repositoryUrl = "https://github.com/biorack/infrastructure_automation"
 
 # Define the local directory where you want to clone or update the repository
-$localDirectory = "C:\Path\To\Your\Local\Directory"
+$localDirectory = "C:\Scripts\"
+
+# Location of repository if installed
+$localRepository = Join-Path -Path $LocalDirectory -ChildPath "infrastructure_automation"
 
 # Check if the local directory exists; if not, clone the repository
-if (-not (Test-Path -Path $localDirectory -PathType Container)) {
+if (-not (Test-Path -Path $localRepository -PathType Container)) {
+
+	# Change to the local directory
+    Set-Location -Path $localDirectory
+	
     # Clone the repository
-    git clone $repositoryUrl $localDirectory
+    git clone $repositoryUrl
 } else {
     # Change to the local directory
-    Set-Location -Path $localDirectory
+    Set-Location -Path $localRepository
 
     # Pull updates from the main branch
     git pull origin main
