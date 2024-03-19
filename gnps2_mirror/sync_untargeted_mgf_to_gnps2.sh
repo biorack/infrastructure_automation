c18ad74dab227f3a17bd8adcb57d0beda5f111a3
Ben Bowen <ben.bowen@gmail.com>
2024-03-19 12:45:03 -0700

    Create sync_untargeted_mgf_to_gnps2.sh

---
+++ gnps2_mirror/sync_untargeted_mgf_to_gnps2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+export PATH="/global/common/software/m2650/lftp/bin:$PATH"
+export LD_LIBRARY_PATH="/global/common/software/m2650/lftp/lib:$LD_LIBRARY_PATH"
+
+# Check if the password argument is provided
+if [ "$#" -ne 1 ]; then
+  source /global/homes/m/msdata/gnps2/gnps2_bpbowen.txt
+  password=$MYVARIABLE
+else
+  password="$1"
+fi
+
+# Define your local and remote directories
+local_directory="/global/cfs/cdirs/metatlas/projects/untargeted_tasks"
+remote_directory="/untargeted_tasks"
+remote_host="sftp.gnps2.org"
+remote_port="6542"
+remote_user="bpbowen"
+
+# Use lftp to synchronize the local and remote directories with the specified port
+
+lftp -u $remote_user,$password sftp://$remote_host:$remote_port << EOF
+mirror --parallel=4 --include "/*.mgf" -R $local_directory $remote_directory
+quit
+EOF
+
+# Explanation:
+# -e: Execute the specified command(s) in lftp
+# mirror -R: Mirror the local directory to the remote directory (recursively)
+# -u: Provide the remote user and password
+# :$remote_port: Specify the custom SFTP port
+# quit: Quit lftp after the mirror operation is complete
+
+# Now, you can provide the SFTP password as a command-line argument when running the script.
